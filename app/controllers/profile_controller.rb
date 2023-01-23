0e47a10530b8ba8c06a6417e6253274ca86f71f8
Can İlgu <canilguu@gmail.com>
2023-01-23 18:39:53 +0300

    aws s3 bucket enabled & tested.

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,8 +6,10 @@ class ProfileController < ApplicationController
   end
 
   def update
-    current_user.update(username: params[:username])
+    profile_params = params.delete_if{ |key,value| key=="authenticity_token" || key=="_method" || key=="controller" || key=="action" || key=="id" || value.blank? }
+    current_user.update(profile_params)
     redirect_to "/profile"
   end
+
 
 end
